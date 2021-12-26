93869e42aa8dd186ff608fc330b3f970ed3d4a02
Jerry Chen <caffreygo@163.com>
2021-12-26 11:57:10 +0800

    [layout]: add different layouts

---
+++ src/layouts/admin.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>admin.vue</h2>
+  <router-view />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/layouts/member.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>member.vue</h2>
+  <router-view />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
