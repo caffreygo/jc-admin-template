10e6b5f8ccfc2dcc264825085b930e6c97ed524c
Jinrui Chen <jinrui@kooboo.cn>
2021-12-29 16:09:45 +0800

    [mock]: use fetch to request API without type intellisense

--- mock/user.ts
+++ mock/user.ts
@@ -2,13 +2,16 @@ import { MockMethod } from 'vite-plugin-mock';
 
 export default [
   {
-    url: '/api/get',
+    url: '/api/user/get',
     method: 'get',
     response: () => {
       return {
-        code: 0,
+        code: 200,
+        message: '请求成功',
+        type: 'success',
         data: {
-          name: 'vben',
+          name: 'Jerry Chen',
+          age: 18,
         },
       };
     },
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
 
 <script setup lang="ts">
 import { env } from './types/helper';
-
+// response: any
+const response = await fetch('http://localhost:3000/api/user/get').then((res) =>
+  res.json()
+);
+console.log(response);
 console.log(env);
 </script>
