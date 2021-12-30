623b3ac619fcf5c283c650f562cce87b02f6fc86
Jinrui Chen <jinrui@kooboo.cn>
2021-12-30 10:57:22 +0800

    [tailwind][💻]: code intellisense by 'Tailwind CSS IntelliSense' and 'HTML CSS Support'

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import userApi from './api/userApi';
+// import { onMounted } from 'vue';
+// import userApi from './api/userApi';
+import { env } from '@/utils/helper';
 
-onMounted(async () => {
-  const response = await userApi.login();
-  console.log(response.data.token);
-});
+console.log(env);
+// onMounted(async () => {
+//   const response = await userApi.login();
+//   console.log(response.data.token);
+// });
 </script>
--- src/plugins/tailwindcss/index.ts
+++ src/plugins/tailwindcss/index.ts
@@ -1,3 +1,5 @@
 import './tailwindcss.css';
 
 export function setupTailwindcss() {}
+
+// vscode type intellisense by 'Tailwind CSS IntelliSense' and 'HTML CSS Support'
