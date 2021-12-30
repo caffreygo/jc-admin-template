b69cc0d2fa7cff1db1004f863b8bf6993754ccda
Jinrui Chen <jinrui@kooboo.cn>
2021-12-30 11:56:34 +0800

    [login][🧩]: basic form and layout astyling

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,7 +1,34 @@
 <template>
-  <div></div>
+  <div class="bg-slate-300 h-screen flex justify-center items-center">
+    <div
+      class="w-[720px] bg-white -translate-y-16 grid grid-cols-2 rounded-md shadow-md overflow-hidden"
+    >
+      <div class="p-6">
+        <h2 class="text-center text-gray-700 text-lg">会员登录</h2>
+        <div class="mt-8">
+          <input
+            type="text"
+            placeholder="请输入手机号或邮箱"
+            class="jc-input"
+          />
+          <input
+            type="text"
+            placeholder="请输入手机号或邮箱"
+            class="jc-input mt-4"
+          />
+        </div>
+      </div>
+      <div>
+        <img src="/images/login.jpg" class="h-80 w-full object-cover" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.jc-input {
+  @apply border border-gray-200 w-full rounded-sm py-1 px-2 outline-none placeholder:text-xs focus:ring-2 ring-offset-2 ring-violet-400 duration-300 focus:border-white;
+}
+</style>
