eb89ea06cb9c3747e7302558db730b68e775c8cb
Jerry Chen <caffreygo@163.com>
2022-01-04 22:51:31 +0800

    [🧸]: simple demo of vee validate - native form element

--- src/views/admin/jc.vue
+++ src/views/admin/jc.vue
@@ -1,14 +1,17 @@
 <template>
   <Form @submit="onSubmit">
     <Field
+      v-slot="{ field, errorMessage }"
       v-model="account"
       name="account"
       :rules="emailRule"
       :validate-on-input="true"
-    />
-    <ErrorMessage name="account" />
-    <br />
-    <button class="border bg-violet-500 text-white mt-2">提交表单</button>
+    >
+      <input v-model="account" v-bind="field" />
+      <hr />
+      <p>{{ errorMessage }}</p>
+    </Field>
+    <button>提交表单</button>
     <!-- <input type="text" /> -->
   </Form>
 </template>
@@ -17,7 +20,7 @@
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import { ref } from 'vue';
 
-const account = ref('123');
+const account = ref<string>('JerryChen');
 const emailRule = (value: any) => {
   return /@/.test(value) ? true : '邮箱格式错误';
 };
@@ -33,6 +36,9 @@ div {
   @apply flex w-screen h-screen justify-center items-center bg-gray-800;
   input {
     @apply border p-2 rounded-md border-violet-500 outline-none;
+  }
+  button {
+    @apply border bg-gray-600 px-4 py-2 rounded-md text-white mt-2;
   }
 }
 </style>
