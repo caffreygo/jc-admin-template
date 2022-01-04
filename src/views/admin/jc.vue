35e89b458543a6a62409a8a3f7316867e9d447a1
Jerry Chen <caffreygo@163.com>
2022-01-04 22:41:47 +0800

    [🧸]: simple demo of vee validate components

--- src/views/admin/jc.vue
+++ src/views/admin/jc.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    <input type="text" />
-  </div>
+  <Form @submit="onSubmit">
+    <Field
+      v-model="account"
+      name="account"
+      :rules="emailRule"
+      :validate-on-input="true"
+    />
+    <ErrorMessage name="account" />
+    <br />
+    <button class="border bg-violet-500 text-white mt-2">提交表单</button>
+    <!-- <input type="text" /> -->
+  </Form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { ref } from 'vue';
+
+const account = ref('123');
+const emailRule = (value: any) => {
+  return /@/.test(value) ? true : '邮箱格式错误';
+};
+
+const onSubmit = (value: any) => {
+  alert('验证通过！');
+  console.log(value);
+};
+</script>
 
 <style lang="scss" scoped>
 div {
