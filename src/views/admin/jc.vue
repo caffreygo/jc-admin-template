e2a7f51cd4aa333dbd367cf6c135432e9d5a503c
Jerry Chen <caffreygo@163.com>
2022-01-04 23:21:11 +0800

    [🧸]: configure vee-validate lang

--- src/views/admin/jc.vue
+++ src/views/admin/jc.vue
@@ -3,6 +3,7 @@
     <Field
       v-slot="{ field, errorMessage }"
       v-model="account"
+      label="账号"
       name="account"
       :rules="{ required: true, email: true }"
       :validate-on-input="true"
@@ -17,14 +18,20 @@
 </template>
 
 <script setup lang="ts">
-import { Form, Field, ErrorMessage, defineRule } from 'vee-validate';
+import { Form, Field, defineRule, configure } from 'vee-validate';
 import { required, email } from '@vee-validate/rules';
 import { ref } from 'vue';
+import { localize } from '@vee-validate/i18n';
+import zh_CN from '@vee-validate/i18n/dist/locale/zh_CN.json';
 
 const account = ref<string>('JerryChen');
 
 defineRule('required', required);
 defineRule('email', email);
+
+configure({
+  generateMessage: localize('zh_CN', zh_CN),
+});
 // const emailRule = (value: any) => {
 //   return /@/.test(value) ? true : '邮箱格式错误';
 // };
