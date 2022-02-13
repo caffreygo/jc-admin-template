de4d113696d641c71b5e6a803827847b30189fc6
Jerry Chen <caffreygo@163.com>
2022-02-13 14:32:07 +0800

    [🧸]: temp

---
+++ src/composables/menu.ts
@@ -0,0 +1,36 @@
+import { IMenu } from '#/menu';
+import { ref } from 'vue';
+import router from '@/router';
+import utils from '@/utils';
+import { CacheEnum } from '@/enum/cacheEnum';
+
+class Menu {
+  public menus = ref<IMenu[]>([]);
+  public history = ref<IMenu[]>([]);
+
+  constructor() {
+    this.menus.value = this.getMenuByRoute();
+    this.history.value = utils.store.get(CacheEnum.HISTORY_MENUS) ?? [];
+  }
+
+  getMenuByRoute() {
+    return router
+      .getRoutes()
+      .filter((route) => route.children.length && route.meta.menu)
+      .map((route) => {
+        const menu: IMenu = { ...route.meta.menu };
+        menu.children = route.children
+          .filter((route) => route.meta?.menu)
+          .map((route) => {
+            return {
+              ...route.meta?.menu,
+              route: route.name,
+            } as IMenu;
+          });
+        return menu;
+      })
+      .filter((menu) => menu.children?.length);
+  }
+}
+
+export default new Menu();
