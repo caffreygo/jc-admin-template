d97a7f9b960342bd2df24db795d49f83a6ac6684
Jerry Chen <caffreygo@163.com>
2022-02-27 16:18:01 +0800

    [🛠]: temp

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,10 +3,10 @@
     class="h-screen w-screen bg-gray-700 flex flex-col justify-center items-center"
   >
     <img
-      src="/images/home.jpg"
+      src="/images/avatar.jpeg"
       class="w-52 h-52 object-cover rounded-full border-8 border-white mb-4"
     />
-    <div>
+    <div class="mt-3">
       <router-link :to="{ name: 'admin.home' }" class="jc-button"
         >后台管理</router-link
       >
@@ -14,22 +14,8 @@
         >用户登录</router-link
       >
     </div>
-    <div class="text-gray-200 text-sm font-mono drop-shadow-md mt-3 px-10">
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum."
+    <div class="text-gray-200 text-sm font-mono drop-shadow-md mt-4 px-10">
+      Impropriety is the soul of wit.
     </div>
-    <el-button type="primary" class="!mt-4">按钮</el-button>
-    <el-icon color="#ffffff" class="mt-2" :size="80">
-      <Bicycle />
-    </el-icon>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { Bicycle } from '@element-plus/icons-vue';
-</script>
