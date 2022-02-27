54c769f55c8ee00f2eafd7329fc161543b559b92
Jerry Chen <caffreygo@163.com>
2022-02-27 12:25:50 +0800

    [🛠]: element与tailwindcss打包后样式冲突处理

--- src/plugins/elementui/index.ts
+++ src/plugins/elementui/index.ts
@@ -1,6 +1,5 @@
 import { App } from 'vue';
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 
 export default function setupElementPlus(app: App) {
   app.use(ElementPlus);
